863f51b1bf084487089e6d2c6720af55e6d94194
Saamiyah Peerun <swaniyah@gmail.com>
2024-10-24 16:44:45 +0400

    update colors

--- packages/frontendmu-nuxt/components/site/LeftMenu.vue
+++ packages/frontendmu-nuxt/components/site/LeftMenu.vue
@@ -1,45 +1,51 @@
 <template>
   <nav
-    class="fixed bottom-[50px] gap-4 flex-col pl-4 justify-end w-10 hidden xl:flex"
+    class="fixed bottom-[50px] gap-4 flex-col pl-4 justify-end w-10 hidden xl:flex z-30"
   >
     <a
+      target="_blank"
       href="https://discord.gg/WxXW9Jvv6k?ref=frontend.mu"
-      class="text-[#5865f2] text-sm md:text-normal hover:animate-bounce"
+      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
     >
       <IconDiscord class="w-6" />
     </a>
 
     <a
+      target="_blank"
       href="https://github.com/Front-End-Coders-Mauritius?ref=frontend.mu"
-      class="text-[#333] text-sm md:text-normal dark:text-white hover:animate-bounce"
+      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
     >
       <IconGithub class="w-6" />
     </a>
 
     <a
+      target="_blank"
       href="https://chat.whatsapp.com/invite/0kQ2QX0ZQ0j1YQ4X6Q4Q4Q"
-      class="text-[#25D366] text-sm md:text-normal hover:animate-bounce"
+      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
     >
       <IconWhatsappCommunity class="w-6" />
     </a>
 
     <a
+      target="_blank"
       href="https://twitter.com/frontendmu"
-      class="text-[#1DA1F2] text-sm md:text-normal hover:animate-bounce"
+      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
     >
-      <IconTwitter class="w-6" />
+      <IconSocialTwitter class="w-6" />
     </a>
 
     <a
+      target="_blank"
       href="https://www.instagram.com/frontend.mu/?ref=frontend.mu"
-      class="text-[#d6249f] text-sm md:text-normal hover:animate-bounce"
+      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
     >
       <IconInstagram class="w-6" />
     </a>
 
     <a
+      target="_blank"
       href="https://www.linkedin.com/company/81846464/admin/?ref=frontend.mu"
-      class="text-[#007db1] text-sm md:text-normal hover:animate-bounce"
+      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
     >
       <IconLinkedin class="w-6" />
     </a>
