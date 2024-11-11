cde3ae2cb93013e711e540e6a19bc938ece4c6e1
the-code-raider <the.raider@outlook.com>
2024-11-11 21:37:23 +0400

    Fixed redirect to github on left menu social icons and added constants

---
+++ packages/frontendmu-nuxt/constants.ts
@@ -0,0 +1,6 @@
+export const GITHUB_URL = 'https://github.com/frontendmu?ref=frontend.mu'
+export const DISCORD_URL = 'https://discord.gg/WxXW9Jvv6k?ref=frontend.mu'
+export const WHATSAPP_URL = 'https://chat.whatsapp.com/invite/0kQ2QX0ZQ0j1YQ4X6Q4Q4Q'
+export const TWITTER_URL = 'https://twitter.com/frontendmu?ref=frontend.mu'
+export const INSTAGRAM_URL = 'https://www.instagram.com/frontend.mu/?ref=frontend.mu'
+export const LINKEDIN_URL = 'https://www.linkedin.com/company/81846464/admin/?ref=frontend.mu'
