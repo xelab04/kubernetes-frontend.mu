49e080c57e0875327ab6c0cbdad45f88e93ec861
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-26 19:22:42 +0400

    refactor: reduce code repetition

--- packages/frontendmu-nuxt/components/site/LeftMenu.vue
+++ packages/frontendmu-nuxt/components/site/LeftMenu.vue
@@ -1,69 +1,33 @@
 <script setup lang="ts">
 import { DISCORD_URL, GITHUB_URL, INSTAGRAM_URL, LINKEDIN_URL, TWITTER_URL, WHATSAPP_URL } from '@/constants'
+
+const socialMediaPlatforms: Record<string, string> = {
+  discord: DISCORD_URL,
+  github: GITHUB_URL,
+  whatsapp: WHATSAPP_URL,
+  twitter: TWITTER_URL,
+  instagram: INSTAGRAM_URL,
+  linkedin: LINKEDIN_URL,
+}
 </script>
 
 <template>
-  <nav
-    class="fixed bottom-[50px] gap-4 flex-col pl-4 justify-end w-10 hidden xl:flex z-30"
-  >
+  <nav class="fixed bottom-[50px] gap-4 flex-col pl-4 justify-end w-10 hidden xl:flex z-30">
     <a
-      aria-label="Discord"
+      v-for="(url, platform) in socialMediaPlatforms"
+      :key="platform"
+      :href="url"
+      :aria-label="platform"
       target="_blank"
-      :href="DISCORD_URL"
       rel="noopener noreferrer nofollow"
       class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
     >
-      <IconDiscord class="w-6" />
-    </a>
-
-    <a
-      aria-label="Github"
-      target="_blank"
-      :href="GITHUB_URL"
-      rel="noopener noreferrer nofollow"
-      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
-    >
-      <IconGithub class="w-6" />
-    </a>
-
-    <a
-      aria-label="Whatsapp"
-      target="_blank"
-      :href="WHATSAPP_URL"
-      rel="noopener noreferrer nofollow"
-      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
-    >
-      <IconWhatsappCommunity class="w-6" />
-    </a>
-
-    <a
-      aria-label="Twitter"
-      target="_blank"
-      :href="TWITTER_URL"
-      rel="noopener noreferrer nofollow"
-      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
-    >
-      <IconSocialTwitter class="w-6" />
-    </a>
-
-    <a
-      aria-label="Instagram"
-      target="_blank"
-      :href="INSTAGRAM_URL"
-      rel="noopener noreferrer nofollow"
-      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
-    >
-      <IconInstagram class="w-6" />
-    </a>
-
-    <a
-      aria-label="LinkedIn"
-      target="_blank"
-      :href="LINKEDIN_URL"
-      rel="noopener noreferrer nofollow"
-      class="text-verse-500 dark:text-verse-200 text-sm md:text-normal hover:animate-bounce hover:ease-in-out hover:delay-200"
-    >
-      <IconLinkedin class="w-6" />
+      <IconDiscord v-if="platform === 'discord'" class="w-6" />
+      <IconGithub v-if="platform === 'github'" class="w-6" />
+      <IconWhatsappCommunity v-if="platform === 'whatsapp'" class="w-6" />
+      <IconSocialTwitter v-if="platform === 'twitter'" class="w-6" />
+      <IconInstagram v-if="platform === 'instagram'" class="w-6" />
+      <IconLinkedin v-if="platform === 'linkedin'" class="w-6" />
     </a>
   </nav>
 </template>
